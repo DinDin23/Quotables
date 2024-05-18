6eaa8ae828e2522c84d6a77eaab4ee174276e473
Dinesh Polisetty <dineshsp2003@gmail.com>
2024-05-18 17:49:27 +0530

    Initial Commit

---
+++ Quotables/QuotablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotablesApp.swift
+//  Quotables
+//
+//  Created by Dinesh Polisetty on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct QuotablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
