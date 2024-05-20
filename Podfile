63b23d8fdebd42bb9b59d79a8abcbc16c4631bad
Dinesh Polisetty <dineshsp2003@gmail.com>
2024-05-20 11:35:03 +0530

    Rest of files

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '17.5'
+use_frameworks!
+
+target 'Quotables' do
+	pod 'FirebaseAnalytics'
+	pod 'FirebaseAuth'
+	pod 'FirebaseFirestore'
+end
--- Quotables/QuotablesApp.swift
+++ Quotables/QuotablesApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct QuotablesApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
